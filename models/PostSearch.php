54b8e7c6eb165ff8bbf8deeefbcdc5a36c22b547
Nikola Basic <basic.nikola@gmail.com>
2014-12-02 15:42:05 +0100

    Added CRUD for Post and Comment
    - added controllers and views for Post model
    - added controllers and views for Comment model

---
+++ models/PostSearch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Post;
+
+/**
+ * PostSearch represents the model behind the search form about `app\models\Post`.
+ */
+class PostSearch extends Post
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['post_id', 'status'], 'integer'],
+            [['title', 'slug', 'description', 'content', 'update_time', 'create_time'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Post::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'post_id' => $this->post_id,
+            'status' => $this->status,
+            'update_time' => $this->update_time,
+            'create_time' => $this->create_time,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
