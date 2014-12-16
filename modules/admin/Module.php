7e248e5130b28d19b014813b816329972f243bfd
Nikola Basic <basic.nikola@gmail.com>
2014-12-16 17:27:51 +0100

    Changed default route in admin module

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -6,10 +6,10 @@ class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'app\modules\admin\controllers';
     public $layout = 'main';
+    public $defaultRoute = 'post';
 
     public function init()
     {
         parent::init();
-
     }
 }
